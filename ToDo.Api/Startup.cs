f1e40c38ce197be06950c41018fb31902d35a4db
Andrea Bertin <andrea.bertin@outlook.com>
2020-05-30 10:49:05 +0200

    #3 SonarQube + Circleci integration

--- ToDo.Api/Startup.cs
+++ ToDo.Api/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ToDo.Api
 {
@@ -20,21 +14,37 @@ namespace ToDo.Api
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// Gets injected app configuration.
+        /// </summary>
+        /// <value></value>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// Gets Startup assembly.
+        /// </summary>
+        /// <returns></returns>
+        public Assembly Asm => GetType().Assembly;
+
+        /// <summary>
+        /// Configures container services.
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwagger(Asm);
+            
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+          
+            app.UseSwaggerWithUI(Asm);
 
             app.UseHttpsRedirection();
 
